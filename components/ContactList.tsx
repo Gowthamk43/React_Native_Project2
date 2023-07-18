fca1333c1e4c71aec7a7148c89ba00a021031692
Gowthamk43 <104433976+Gowthamk43@users.noreply.github.com>
2023-07-18 15:17:41 +0530

    name Changes

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -8,7 +8,7 @@ export default function ContactList() {
         {
 
             id: 1,
-            name: 'Gowtham',
+            name: 'Gowtham Krishna',
             status: 'InActive',
             image: 'https://plus.unsplash.com/premium_photo-1676389763792-4519eaf7c4cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
         }, {
